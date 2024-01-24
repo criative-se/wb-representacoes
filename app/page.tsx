f14a9dfc29194a7d9e1a896b088f5f2a1f3cf8a3
Gustavo Silva Martins <gustavosm994@gmail.com>
2024-01-24 14:54:17 -0300

    fix: device zoom

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="sm:hidden md:hidden lg:block relative w-[60rem] h-[40rem] mt-20">
+            <div className="sm:hidden md:hidden lg:block relative w-[60rem] h-[39rem] mt-24">
               <Image src='/hero.png' alt='Wilson Bittencourt' fill style={{ objectFit: 'cover', objectPosition: 'left' }} />
               <Image src='/selo.png' alt="+12 anos de experiência" width={320} height={320} style={{ position: 'absolute', right: 0, top: 32 }} />
             </div>
